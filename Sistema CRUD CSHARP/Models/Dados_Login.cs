133a923da3ede9255abaf9f0b2a485a6775fd2b4
wils0nalves <yuri.tinocoalves@gmail.com>
2023-01-23 10:04:40 -0300

    Adicionar arquivos de projeto.

---
+++ Sistema CRUD CSHARP/Models/Dados_Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_CRUD_CSHARP.Models
+{
+    internal class Dados_Login
+    {
+        public Dados_Login(int id, string login, string email, string senha, bool ativo)
+        {
+            Id = id;
+            Login = login;
+            Email = email;
+            Senha = senha;
+            Ativo = ativo;
+        }
+
+        public Dados_Login() : this(0, "", "", "", true) { }
+
+        public int Id { get; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public bool Ativo { get; set; }
+
+    }
+}
